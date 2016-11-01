7e4da4f6c79b1ee9211e0fb63d27a93667f6f2af
jahilliard <justin.a.hilliard@gmail.com>
2016-11-01 07:56:32 -0700

    added request manager

---
+++ request_manager/RequestManager.py
@@ -0,0 +1,22 @@
+import requests as r
+
+class RequestManager:
+
+    @staticmethod
+    def get(url):
+        resp = r.get(url)
+        if RequestManager.confirm_response(resp):
+            try:
+                return resp.json()
+            except ValueError:
+                return resp.decode('utf-8')
+        else:
+            raise Exception('status code not 200')
+
+    @staticmethod
+    def confirm_response(resp):
+        if 199 < resp.status_code < 300:
+            return True
+        else:
+            print("Status Code: " + str(resp.status_code))
+            return False
